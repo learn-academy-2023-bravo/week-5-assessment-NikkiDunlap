ce4dfea50e69c158b66e693e5b8e8366b9712b9e
LEARN Academy <sdunlap102326@gmail.com>
2023-04-16 21:29:24 -0700

    Completed Test 5

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,42 +11,130 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+    // Pseudocode:
+        // Create a describe function named encodedString
+        // Create 4 tests that describe different situations that the function should be able to test for
+        // Use the given variables to make sure the expected output is correct
+        // Test 2 checks to see if our function will return an empty string when it's given an input of an empty sting
+        // Test 3 checks to see if our function will give us the correct output when we are given a string with no vowels as an input
+        // Test 4 checks 
+        // Use the .toBe to check whether the output matches the expected output when our input has lowercase and uppercase letters
+        // Use "for" loop to iterate through each character in our input string. Use .toLowerCase to in order for the function to pick up lowercase and uppercase letters in our string. If the character is one of the vowels listed, it will replace it with the encoded value
+        // Return the encodedStr using our expected outputs
+
 
 // a) Create a test with expect statements using the variables provided.
-
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
-
-// b) Create the function that makes the test pass.
-
+describe("encodedString", () => {
+    it("returns a string with a coded message", () => {
+        const secretCodeWord1 = "Lackadaisical"
+        // Expected output: "L4ck4d41s1c4l"
+        const secretCodeWord2 = "Gobbledygook"
+        // Expected output: "G0bbl3dyg00k"
+        const secretCodeWord3 = "Eccentric"
+        // Expected output: "3cc3ntr1c"
+        expect(encodedString(secretCodeWord1)).toBe("L4ck4d41s1c4l")
+        expect(encodedString(secretCodeWord2)).toBe("G0bbl3dyg00k")
+        expect(encodedString(secretCodeWord3)).toBe("3cc3ntr1c")})
+    it("should return an empty string when given an empty string", () => {
+        expect(encodedString("")).toBe("")
+    })
+    it("should return strings without vowels", () => {
+        expect(encodedString("xyz")).toBe("xyz")
+    })
+    it("should return strings with upper and lowercase vowels", () => {
+        expect(encodedString("CaT")).toBe("C4T")
+    })
+})
+    // b) Create the function that makes the test pass.
+        const encodedString = (string) => {
+            const vowels = {"a": "4", "e": "3", "i": "1", "o": "0"}
+        let encodedStr = ("")
+        for (let i = 0; i < string.length; i++) {
+            let char = string[i].toLowerCase();
+            if (vowels[char]) {
+              encodedStr += vowels[char];
+            } else {
+              encodedStr += string[i];
+            }
+          }
+          
+          return encodedStr;
+        }
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
-
+        // Pseudocode:
+            // Given an array of fruits 
+            // Create 4 tests that describe different situations that the function should be able to test for
+            // Use the given variables to make sure the expected output is correct
+            // Create 2 test cases that define the variables 'letterA' and 'letterE' to filter the array
+            // Define our 'givenExpectedOutput' variable so it contains the expected output 
+            // Define the filterTheWords function that takes an array of strings array and a string letter as input parameters
+            // Use the "filter" method to create a new array that contains ONLY the string that "include" the specific letter that out outputs call for
+            // Return the filterTheWords function to return the filtered array
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
-
-// b) Create the function that makes the test pass.
+describe("filterTheWords", () => {
+    const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+    it("returns an array of words with the letter 'a' in it", () => {
+        const letterA = "a"
+        const givenExpectedOutput = ["Mango", "Apricot", "Peach"]
+        const ourResult = filterTheWords(fruitArray, letterA)
+        expect(ourResult).toEqual(givenExpectedOutput)
+    })
+   it("returns an array of words with the letter 'e' in it", () => {
+    const letterE = 'e'
+    const givenExpectedOutput = ["Cherry", "Blueberry", "Peach"]
+    const ourResult = filterTheWords(fruitArray, letterE)
+    expect(ourResult).toEqual(givenExpectedOutput)
+   })
+})
+    // b) Create the function that makes the test pass.
+    const filterTheWords = (array, letter) => {
+        return array.filter((word) => word.includes(letter))
+    }
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+    // Pseudocode:
+        // Create a function named 'isAFullHouse' that takes in an array of numbers and returns true is the hand is a fullhouse and it will return false if it is not
+        // Create two sets of test where the first test checks to see if the function with the given arrays are hands with a fullhouse
+        // The second test will check the given arrays with a parameter set to false for a hand that is not a fullhouse
+        // Create a passing test that sorts the hand in order using .sort()
+        // Create an object called counts
+        // Create a "for" loop that iterates over each card in the hand, checking the value of each card
+        // Create an array called values that counts the value of each card in the hand
+        // Use the Object.value method to get the vaues of the 'counts' object.
+        // Use .length to check if the length of the values array is 2 because there are only 2 unique card values in the hand
+        // If it === to 3 instead of 2, the function will return true. If not, it will return false
+        // Get expected output
+        // I truly struggled with this one, and I am not sure if I used the right technique to get an answer
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe("isAFullHouse", () => {
+    it("returns true for a fullhouse")
+        const hand1 = [5, 5, 5, 3, 3]
+        const firstResult = isAFullHouse(hand1)
+        expect(firstResult).toEqual(true)
 
+        const hand4 = [7, 2, 7, 2, 7]
+        const fourthResult = isAFullHouse(hand4)
+        expect(fourthResult).toEqual(true)
+})
+    it("returns false for a hand that is not a fullhouse", () => {
+        const hand3 = [5, 5, 5, 5, 4]
+        const thirdResult = isAFullHouse(hand3)
+        expect(thirdResult).toEqual(false)
+
+        const hand2 = [5, 5, 3, 3, 4]
+        const secondResult = isAFullHouse(hand2)
+        expect(secondResult).toEqual(false)
+    })
 // b) Create the function that makes the test pass.
+const isAFullHouse = (hand) => {
+    hand.sort((a, b) => a - b);
+    const counts = {};
+    for (const card of hand) {
+      counts[card] = (counts[card] || 0) + 1;
+    }
+    const values = Object.values(counts);
+    return values.length === 2 && (values[0] === 2 || values[0] === 3);
+  }
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,17 +10,75 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+# Pseudocode: 
+    # Define a function named 'includes_this_letter' that has 2 parameters 'array_of_words' and 'letter'
+    # Create an empty array named 'our_result' to hold the words that contain the 'letter' we are looking for 
+    # Iterate over each word in the given array by using .each
+    # Check if each word contains the letter in our given variables by using the .include? method
+    # If it does have the letter in our given variable, the string will be added to 'our_result' using <<
+    # The return will consist of the array containing the words that include the given letters
+
+        def includes_this_letter(array_of_words, letter)
+            our_result = []
+            array_of_words.each do |word|
+            if word.include?(letter)
+                our_result << word
+            end
+        end
+            return our_result
+        end
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+us_states = 
+{ northwest: ['Washington', 'Oregon', 'Idaho'], 
+southwest: ['California', 'Arizona', 'Nevada'], 
+notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
+
+        # Pseudocode:
+            # Define a method called "sort_array_no_nested" that takes in "hash" as a single argument
+            # Create an empty array called values from the new hash that we will have to sort and un-nest
+            # Use .each to iterate over each kay value pair within the hash
+            # The key variable is to represent the given value array
+            # Use the .concat method to add all of the values in the values array. We have to use this instead of push so we don't produce nested arrays
+            # Use the .sort method to sort the method in alphabetical order 
+
+            def sort_array_no_nested(hash)
+                values = []
+                hash.each do |key, value|
+                    values.concat(value)
+                end
+                values.sort
+            end
+
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+
+    # Pseudocode:
+            # Create a class called "Bike"
+            # Use the attr_accessor to define the 3 instance variables named "model", "wheels", and "current_speed." This is a shortcut for making getter and setter methods
+            # Initialize method takes 3 arguments and sets the value of the instance variables using "@"
+            # Create a variable called "bike_info" that uses string interpolation to make a string that details information about the bike
+
+            class Bike
+                attr_accessor :model, :wheels, :current_speed
+                def initialize(model, wheels = 2, current_speed = 0)
+                    @model: model
+                    @wheels: wheels
+                    @current_speed: current_speed
+                end
+
+                def bike_info
+                    "The #{model} has #{wheels} wheels and is going #{current_speed} mph."
+                end
+                
+            end
 
 
 
@@ -30,3 +88,31 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+    #Pseudocode:
+            # Define "pedal_faster" method
+            # pedal_faster takes in "amount" as the argument. This method increases the current speed by taking the amount value and adding it to the current speed instance variable
+            # Define "brake" method with the argument of "amount." This method reduces the current speed and checks to see what the end speed is. If the number is less than zero, sets it to zero because there cannot be a negative speed
+
+            class Bike
+                attr_accessor :model, :wheels, :current_speed
+              
+                def initialize(model, wheels = 2, current_speed = 0)
+                  @model = model
+                  @wheels = wheels
+                  @current_speed = current_speed
+                end
+              
+                def bike_info
+                  "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+                end
+              
+                def pedal_faster(amount)
+                  @current_speed += amount
+                end
+              
+                def brake(amount)
+                  @current_speed = [@current_speed - amount, 0].max
+                end
+              end
+            
